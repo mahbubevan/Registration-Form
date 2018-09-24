989d8f0c49f0dc30ad2c2f9654ed39b64c8ad82e
Mahbub Alam Evan <mahbubalamevan@Mahbubs-MacBook-Air.local>
2018-09-24 14:15:16 +0600

    jQuery UI Complete

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-$(document).ready(function(){
-  $(window).load(function() {
-		// Animate loader off screen
-		$(".se-pre-con").fadeOut("slow");;
-	});
-});
+var bloodGroup = ['A+','B+','O+','A-','B-','O-','AB+','AB-']
+
+var bloodGroupOption = document.getElementById('bloodInput');
+bloodGroupOption.innerHTML = "";
+
+
+for (var i=0;i<bloodGroup.length;i++){
+  console.log(bloodGroup[i]);
+  bloodGroupOption.innerHTML += '<option value="\''+ bloodGroup[i] +'\'">'+bloodGroup[i]+
+                                '</option>';
+}
